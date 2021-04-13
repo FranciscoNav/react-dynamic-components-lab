faaee17da2db82914ffccec090f07680298159b9
Francisco <fnaveira11@gmail.com>
2021-04-13 14:06:16 -0400

    commit tree cdc1716aa4018a40a4139f97840478ad26ad79d7

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -10,9 +10,11 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+     if (this.props.opacity > 0.2 ){
+      <div className="color-box" style={{opacity:0.2}}>
+      <ColorBox opacity = {this.props.opacity} />
       </div>
+     }else{ null }
     )
   }
 
